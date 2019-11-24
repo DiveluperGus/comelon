4101d6833fc629e7548c6c981c6537741ea8b218
Gustavo Reyes Martínez <greyes@mediomelon.mx>
2019-11-24 01:47:30 -0600

    Creación de pantalla SplashScreen y LoginActivity

--- app/src/main/java/com/gustavo/comelon/SplashActivity.java
+++ app/src/main/java/com/gustavo/comelon/SplashActivity.java
@@ -1,15 +1,53 @@
 package com.gustavo.comelon;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
+
+    @BindView(R.id.progress_bar_splashScreen)
+    ProgressBar progressBar;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //Comentario
+
+        ButterKnife.bind(this);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    //Update the progress bar and display
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            if (progressStatus == 100) {
+                                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(i);
+                                finish();
+                            }
+                        }
+                    });
+                    try {
+                        Thread.sleep(20); //100*20 = 200 mls = 2 segundos
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
